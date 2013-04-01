e86e427d72819269fe9162b34b682dfc99cd3766
Phillip C Clark <eowdaoc@aol.com>
2013-03-31 23:23:36 -0500

    View_semester structure complete

--- application/views/admin/view_semester.blade.php
+++ application/views/admin/view_semester.blade.php
@@ -12,21 +12,114 @@
 </head>
 
 <body>
-  <div id="logo_div">
+  <div id="logo-div">
     <?php echo HTML::image( "img/LOGOBIG.jpg",
                             "UNA",
-                            array("id" => "logo_img"));
+                            array("id" => "logo-img"));
     ?>
   </div>
 
-  <div id="main_div">
+  <div id="main-div">
 
+    <div id="menu-div">
+      <div class="menu-btn" id="versions-btn">
+        <div class="option-txt">schedule versions</div>
+      </div>
+
+      <div class="menu-btn" id="input-btn">
+        <div class="option-txt">input files</div>
+      </div>
+
+      <div class="menu-btn" id="pref-btn">
+        <div class="option-txt">preferences</div>
+      </div>
+    </div>
+
+    <!--//////////////////////////////////////-->
+    <div id="welcome-div" class="container">
+      <div id="welcome-txt">
+        Welcome to the administrator control panel.
+      </div>
+    </div>
+
+    <!--//////////////////////////////////////-->
+    <div id="versions-div" class="container">
+    </div>
+
+    <!--//////////////////////////////////////-->
+    <div id="input-div" class="container">
+
+      <!-- --------------------------------- -->
+      <div id="class-time-btn" class="file-btn">
+        class times
+      </div>
+
+      <div id="class-time-div" class="file-div">
+        <textarea></textarea>
+      </div>
+
+      <!-- --------------------------------- -->
+      <div id="room-btn" class="file-btn">
+        available rooms
+      </div>
+
+      <div id="room-div" class="file-div">
+        <textarea></textarea>
+      </div>
+
+      <!-- --------------------------------- -->
+      <div id="course-btn" class="file-btn">
+        courses to schedule
+      </div>
+
+      <div id="course-div" class="file-div">
+        <textarea></textarea>
+      </div>
+
+      <!-- --------------------------------- -->
+      <div id="conflict-btn" class="file-btn">
+        conflict times
+      </div>
+
+      <div id="conflict-div" class="file-div">
+        <textarea></textarea>
+      </div>
+
+      <!-- --------------------------------- -->
+      <div id="prereq-btn" class="file-btn">
+        prerequisites
+      </div>
+
+      <div id="prereq-div" class="file-div">
+        <textarea></textarea>
+      </div>
+
+      <!-- --------------------------------- -->
+      <div id="faculty-btn" class="file-btn">
+        faculty members
+      </div>
+
+      <div id="faculty-div" class="file-div">
+        <textarea></textarea>
+      </div>
+
+    </div>
+
+    <!--//////////////////////////////////////-->
+    <div id="pref-div" class="container">
+
+
+
+
+
+    </div>
+
+  </div>
 
     <?php
       // Can take the following out. Just playing around with passing data. 
       echo "Currently viewing " . $semester 
     ?>
-  </div>
 </body>
 
 </html>
--- public/js/view_semester.js
+++ public/js/view_semester.js
@@ -1,3 +1,41 @@
 $(document).ready(function(){
 
+  // Hide all pages initially
+  $("#versions-div").hide();
+  $("#input-div").hide();
+  $("#pref-div").hide();
+
+  $(".file-div").hide();
+
+  // Toggle pages
+  $("#versions-btn").on("click",showVersions);
+  $("#input-btn").on("click",showInput);
+  $("#pref-btn").on("click",showPref);
 });
+
+function showVersions(){
+
+  // Display the versions div
+  
+  $(".container").hide();
+  $("#versions-div").show();
+
+}
+
+function showInput(){
+
+  // Display the input div
+
+  $(".container").hide();
+  $("#input-div").show();
+}
+
+function showPref(){
+
+  // Display the preferences div
+
+  $(".container").hide();
+  $("#pref-div").show();
+}
+
+
