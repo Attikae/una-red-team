97a419a5beb076e16ef2e6e4c1f358fbb2e4551f
Jordan Shook <jordandavisshook@gmail.com>
2013-04-09 11:50:20 -0500

    Available Rooms Scanner
    Needs to be tested.

--- application/models/available_room.php
+++ application/models/available_room.php
@@ -2,23 +2,69 @@
 
 class Available_Room extends Eloquent {
 
-  public static $table = 'available_rooms';
+    public static $table = 'available_rooms';
 
-  public static $timestamps = true;
+    public static $timestamps = true;
 
-  public static function scan($schedule_id, $file_string){
+    public static function scan($schedule_id, $file_string){
 
-    /* Return an array called $result with the indices status & message.
-       Set $result['status'] equal to 'success' if everything goes as planned.
-       Set $result['status'] equal to 'error' if there is an issue.
-       If there is an issue, set result['message'] to a string containing the line number
-       and description of the issue */
-
-       // For testing purposes
-       $result = array("status" => "error", "message" => "available room test");
-
-       return $result;
-
-  }
+        /* Return an array called $result with the indices status & message.
+        Set $result['status'] equal to 'success' if everything goes as planned.
+        Set $result['status'] equal to 'error' if there is an issue.
+        If there is an issue, set result['message'] to a string containing the line number
+        and description of the issue */
+        
+        $lineArray = mb_split("\n", $file_string);
+        $success = true;
+        $result = array();
+        
+        for ($i = 0; $i < count($lineArray); $i++)
+        {
+            $wordArray[$i] = mb_split (" ", $lineArray[$i]);
+             
+            if($wordArray[$i][0] != 'C' && $wordArray[$i][0] != 'L' && $wordArray[$i][0] != 'B'){
+                $success = false;  
+                $result['status'] = "error";
+                $result['message'] =   $result['message'] . "\nIncorrect type of room on line " . $i .  ". ";
+            }
+            if($wordArray[$i][1] > 100 || $wordArray[$i][1] < 1){
+                $success = false;
+                $result['status'] = "error";
+                $result['message'] =   $result['message'] . "\nIncorrect size of room on line " . $i . ". ";
+            }
+            if(!mb_ereg_match('^[A-Z]+$', $wordArray[$i][2]) || strlen($wordArray[$i][2]) > 6 || strlen($wordArray[$i][2])< 2)
+            {
+                $success = false;
+                $result['status'] = "error";
+                $result['message'] =  $result['message']  . "\nIncorrect building name on line " . $i . '. '; 
+            }
+            if(!mb_ereg_match('^[0-9]+$', $wordArray[$i][3]) || strlen($wordArray[$i][3]) < 1 || strlen($wordArray[$i][3]) > 3)
+            {
+                $success = false;
+                $result['status'] = "error";
+                $result['message'] =  $result['message']  . "\nIncorrect room number on line " . $i . '. '; 
+            
+            }    
+        }
+        
+        if ($success == true)
+        {
+            $result['status'] = "success";
+            
+            for($count = 0; $count < count($wordArray); $count++)
+            {
+                $new_room = new Available_Room;
+                $new_room->schedule_id = $schedule_id;
+                $new_room->type = $wordArray[$count][0];
+                $new_room->size = $wordArray[$count][1];
+                $new_room->building = $wordArray[$count][2];
+                $new_room->room_number = $wordArray[$count][3];
+                $new_room->save();
+            } 
+        }
+        
+        return $result;
+        
+    }
   
 }
