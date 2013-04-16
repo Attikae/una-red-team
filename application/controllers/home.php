daa004f71ab197104c110bea8b8fa9c35f4b08be
Ash Karki <akarki@una.edu>
2013-04-16 15:36:52 -0500

    password validation

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -82,7 +82,7 @@ class Home_Controller extends Base_Controller {
                             return Redirect::to('home/changepw'); //need this page setup
                         }
                         
-                        return Redirect::to('faculty/faculty_index'); //need this page setup
+                        return Redirect::to('faculty/faculty_index');
                         
                     }
                     else if ( ($user->user_type == 1) || ($user->user_type == 3) )
@@ -144,34 +144,36 @@ class Home_Controller extends Base_Controller {
         $new_pw = $_POST['new-pw'];
         $repeat_new_pw = $_POST['rpt-new-pw'];
 
-        if($user->password != $old_pw) //check if current password match
-            $message["error"] = "Password Invalid!</br>"; 
+        if(!$old_pw)
+            $message["error"] = "Enter your current password";    
+        else if($user->password != $old_pw) //check if current password match
+            $message["error"] = "Password Invalid!</br>";
+        else if(!$new_pw)
+            $message["error"] = "New password required!"; 
+        else if(!$repeat_new_pw)
+            $message["error"] = "Re-enter new password!";
         else if($new_pw != $repeat_new_pw) //check is new and repeat password match
             $message["error"] = "Password Mismatch!</br>";
         else
         {
             if(strlen($new_pw) < 6)//check for pw less than 6 char
                 $message["error"] = "Password must be atleast 6 character long!</br>";
-            else if (count($new_pw) > 10)//check for pw longer than 10 char
+            else if (strlen($new_pw) > 10)//check for pw longer than 10 char
                 $message["error"] = "Password cannot be longer than 10 character!</br>";
-            else if(!ctype_alpha(substr($new_pw, 0)))//check if first char is not alpha
+            else if(!ctype_alpha($new_pw[0]))//check if first char is not alpha
                 $message["error"] = "Password must begin with alphabet!</br>";
             else
             {
-                if(!preg_match('/[^0-9A-Za-z!.,?]/', $new_pw))//check if all the char are valid char
-                    $message["error"] = "Password can only contain . , ! ? 0-9 a-z A-Z'</br>";  
-                else if(!preg_match('/.,?!', $new_pw))//check is pw do contain at least one valid symbols
+                if(!preg_match("/^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z]).*$/", $new_pw))//check if all the char are valid char
+                    $message["error"] = "Password can only contain . , ! ? 0-9 a-z A-Z</br>";  
+                else if(!preg_match('^[/.,?!]+', $new_pw))//check is pw do contain at least one valid symbols
                     $message["error"] = "Password must contain atleast one , or . or ! or ?";
                 else
                 {
                     $user->password = $new_pw;  //set new pw
                     $user->save();              //save new pw
                     $message["error"] = "Password changed! Login with new password.";
-                        
-                    /* if($user->user_type == 2)  //redirect to admin
-                        return Redirect::to('admin/admin_index');
-                    else if($user->user_type == 3)//redirect to faculty
-                        return Redirect::to('faculty/faculty_index'); */
+                   
 
                     return View::make('home.login')->with("message", $message);
                 }
