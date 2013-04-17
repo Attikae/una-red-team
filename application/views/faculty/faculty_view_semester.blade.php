444a2e5fdd369f1c6e9c05cdaf32c7da646b58dd
Phillip C Clark <eowdaoc@gmail.com>
2013-04-17 18:42:22 -0500

    Added view semester page for faculty, tweaked faculty.php

---
+++ application/views/faculty/faculty_view_semester.blade.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+
+  <title>UNA CLASS SCHEDULER</title>
+
+  {{ HTML::script("js/jquery.js") }}
+  {{ HTML::style("css/faculty_view_semester.css") }}
+  {{ HTML::script("js/faculty_view_semester.js") }}
+</head>
+
+<body>
+  <iframe id="file-submit-iframe" name='file-submit-iframe' style='display: none;'></iframe>.
+  <div id="logo-div">
+    <?php echo HTML::image( "img/LOGOBIG.jpg",
+                            "UNA",
+                            array("id" => "logo-img"));
+    ?>
+  </div>
+
+  <div id="top-menu">
+    <a id="home-lnk" href="faculty_index">home</a>
+    <br>
+    
+  </div>
+
+  <div id="semester-title">
+    <?php
+      // Can take the following out. Just playing around with passing data. 
+      echo "Currently viewing " . Session::get('semester'); 
+    ?>
+  </div>
+
+  <?php echo '<input id="schedule_id" type="hidden" value="' . Session::get('schedule_id') . '"></input>'; ?> 
+
+  <div id="main-div">
+
+    <div id="menu-div">
+      <div class="menu-btn" id="versions-btn">
+        <div class="option-txt">schedule versions</div>
+      </div>
+
+      <div class="menu-btn" id="pref-btn">
+        <div class="option-txt">preferences</div>
+      </div>
+    </div>
+
+    <!--////////////////////////////////////////////////////////////-->
+    <div id="welcome-div" class="container">
+      <div id="welcome-txt">
+        Welcome to the semester control panel.
+      </div>
+    </div>
+
+    <!--////////////////////////////////////////////////////////////-->
+    <div id="versions-div" class="container">
+
+      <table id="versions-list">
+
+        <?php
+
+          if(empty($versions))
+          {
+            echo "<tr><th>No versions currently exist.</th></tr>";
+          }
+          else
+          {
+            foreach($versions as $version)
+            {
+              echo "<tr><td><span id='". $version->id ."' class='version-lnk'>"
+                   . $version->name
+                   . "</span></td></tr>";
+            }
+          }
+
+        ?>
+      </table>
+    </div>
+    <!--////////////////////////////////////////////////////////////-->
+    <div id="pref-div" class="container">
+
+      <h1 id="pref-title">course preferences</h1>
+      <div class="course-div">
+        <div class="course-txt">
+          CS 155
+        </div>
+
+        <div class="checkbox-grp">
+          <div class="checkbox-txt">
+            morning
+            <input type="checkbox"></input>
+          </div>
+
+          <div class="checkbox-txt">
+            afternoon
+            <input type="checkbox"></input>
+          </div>
+
+          <div class="checkbox-txt">
+            evening
+            <input type="checkbox"></input>
+          </div>
+        </div>
+      </div>
+    
+      <div class="course-div">
+        <div class="course-txt">
+          CS 245
+        </div>
+
+        <div class="checkbox-grp">
+          <div class="checkbox-txt">
+            morning
+            <input type="checkbox"></input>
+          </div>
+
+          <div class="checkbox-txt">
+            afternoon
+            <input type="checkbox"></input>
+          </div>
+
+          <div class="checkbox-txt">
+            evening
+            <input type="checkbox"></input>
+          </div>
+        </div>
+      </div>
+
+      <div class="course-div">
+        <div class="course-txt">
+          CS 255
+        </div>
+
+        <div class="checkbox-grp">
+          <div class="checkbox-txt">
+            morning
+            <input type="checkbox"></input>
+          </div>
+
+          <div class="checkbox-txt">
+            afternoon
+            <input type="checkbox"></input>
+          </div>
+
+          <div class="checkbox-txt">
+            evening
+            <input type="checkbox"></input>
+          </div>
+        </div>
+      </div>
+
+      <div class="course-div">
+        <div class="course-txt">
+          CS 355
+        </div>
+
+        <div class="checkbox-grp">
+          <div class="checkbox-txt">
+	    morning
+	    <input type="checkbox"></input>
+          </div>
+
+            
+          <div class="checkbox-txt">
+	    afternoon
+            <input type="checkbox"></input>
+          </div>
+        
+
+          <div class="checkbox-txt">
+	    evening
+            <input type="checkbox"></input>
+          </div>
+            
+        </div>
+      </div>
+
+      <div id="pref-btn-div">
+        <button id="pref-submit-btn">submit</button>
+        <button id="pref-reset-btn">reset</button>
+      </div>
+    </div>
+  </div>
+
+    
+</body>
+
+</html>
+
---
+++ public/js/faculty_view_semester.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+  // Hide all pages initially
+  $("#versions-div").hide();
+  $("#input-div").hide();
+  $("#pref-div").hide();
+
+  // Toggle pages
+  $("#versions-btn").on("click",showVersions);
+  $("#input-btn").on("click",showInput);
+  $("#pref-btn").on("click",showPref);
+
+
+  $(".version-lnk").on("click",function(){
+    return false;
+  });
+});
+
+function showVersions(){
+
+  // Display the versions div
+  
+  $(".container").hide();
+  $("#versions-div").show();
+
+}
+
+function showInput(){
+
+  // Display the input div
+
+  $(".container").hide();
+  $("#input-div").show();
+}
+
+function showPref(){
+
+  // Display the preferences div
+
+  $(".container").hide();
+  $("#pref-div").show();
+}
