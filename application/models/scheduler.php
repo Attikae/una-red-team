915f7dfbdbf376b866012090ceb8c3f05840634d
Phillip C Clark <eowdaoc@gmail.com>
2013-04-21 15:25:17 -0500

    Started scheduling helper functions

--- application/models/scheduler.php
+++ application/models/scheduler.php
@@ -13,5 +13,36 @@ class Scheduler {
 
   }
 
+  public static function get_faculty_list( $schedule_id, $priority_bool )
+  {
+    // If priority_bool is 0, use seniority
+    // If priority_bool is 1, use submission
 
+    $faculty_list;
+
+    if( $priority_bool == 0 )
+    {
+      $faculty = Faculty_Member::where_schedule_id($schedule_id)
+                    ->order_by('years_of_service','desc')->get();
+    }
+    else
+    {
+      $faculty = Faculty_Member::where_schedule_id($schedule_id)
+                    ->order_by('updated_prefs_at','desc')->get();
+    }
+
+    $i = 0;
+
+    foreach( $faculty as $x )
+    {
+      $faculty_list[i]->faculty_id = $x->id;
+      $faculty_list[i]->hours = $x->hours;
+
+      $prefs = Faculty_Preference::where_faculty_id($x->id)
+                  ->where_schedule_id($schedule_id)->get();
+
+
+      $i = $i + 1;
+    }
+  }
 }
