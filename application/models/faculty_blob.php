a17cc06ba87f73655bffa2ddd3bab11ce85434a2
Atticus Wright <atticuswright@gmail.com>
2013-04-21 13:47:40 -0500

    Added models for scheduling algorithm

---
+++ application/models/faculty_blob.php
@@ -0,0 +1,11 @@
+<?php
+
+class Faculty_Blob {
+
+  public $faculty_id;
+  public $sections;
+  public $day_prefs;
+  public $hours;
+
+
+}
---
+++ application/models/time_blob.php
@@ -0,0 +1,36 @@
+<?php
+
+class Time_Blob {
+
+  public $class_time_id;
+  public $start_offset;
+  public $end_offset;
+  public $credit_hours;
+  public $m;
+  public $t;
+  public $w;
+  public $r;
+  public $f;
+  public $s;
+
+
+  public static get_time_blobs(){
+
+    get all class times from fbsql_database
+
+    $time_blobs;
+
+    for each class time(
+
+      
+    )
+
+
+
+
+  }
+
+}
+
+
+$time_blobs = Time_Blob::get_time_blobs()
