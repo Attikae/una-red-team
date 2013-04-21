2e19bd21dd4ab1639946fa28d0a205e6529f3330
Atticus Wright <atticuswright@gmail.com>
2013-04-21 14:19:47 -0500

    Update to scheduling algorithm code

--- application/models/scheduler.php
+++ application/models/scheduler.php
@@ -2,7 +2,16 @@
 
 class Scheduler {
 
-  
+  public static function schedule_driver($schedule_id, )
+  {
+
+    $schedule = Schedule::find($schedule_id);
+    $time_string = date('m-d-Y H:i:s');
+    $name = $schedule->name . " " $schedule->year . " " . $time_string;
+
+    $output_version = Output_Version::create(array())
+
+  }
 
 
 }
