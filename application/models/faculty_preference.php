006a0d3aedecfa7f17c5a325fcbe052df47a10b4
Atticus Wright <atticuswright@gmail.com>
2013-04-21 20:54:05 -0500

    Added function shell for automatically filling faculty_preferences table

--- application/models/faculty_preference.php
+++ application/models/faculty_preference.php
@@ -6,4 +6,20 @@ class Faculty_Preference extends Eloquent {
 
   public static $timestamps = true;
 
+
+  puplic static function fill_prefs($schedule_id)
+  {
+
+    $faculty_members = Faculy_Member::where_schedule_id($schedule_id);
+    $courses = Course_To_Schedule::where_schedule_id($schedule_id);
+
+    foreach ($faculty_members as $faculy_member) {
+      foreach($courses as $course){
+        //Create faculty preference entries
+      }
+    }
+
+
+  }
+
 }
