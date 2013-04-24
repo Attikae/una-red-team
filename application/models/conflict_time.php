fd3aacd6cbfeb3d9cd764a7825a602386ec22125
Jason Smith <unalions37@gmail.com>
2013-04-23 21:06:36 -0500

    conflict time scanner documented

--- application/models/conflict_time.php
+++ application/models/conflict_time.php
@@ -5,26 +5,42 @@ class Conflict_Time extends Eloquent {
     public static $table = 'conflict_times';
 
     public static $timestamps = true;
+	
+    /**************************************************************************
+    /* @function    scan
+    /* @author      Jordan Shook
+    /* @description This segment of code will scan an incoming file(the format
+    /*              of which can be found in section A.4 of the specification
+    /*              document) to ensure the correct format is found.
+    /* @input       $schedule_id -> the identification number of the schedule
+    /*              currently being created. This value is used when inputing 
+    /*              to and extracting from the database.
+    /*              @file_string -> the string that holds the information to be
+    /*              processed by the scanner.
+    /* @output      Returns an array called $result with the indices status & 
+    /*              message. Set $result['status'] equal to 'success' if 
+    /*              everything goes as planned. Set $result['status'] equal to 
+    /*              'error' if there is an issue. If there is an issue, set 
+    /*              result['message'] to a string containing the line number 
+    /*              and description of the issue.
+    **************************************************************************/
 
     public static function scan($schedule_id, $file_string){
 
-    /* Return an array called $result with the indices status & message.
-       Set $result['status'] equal to 'success' if everything goes as planned.
-       Set $result['status'] equal to 'error' if there is an issue.
-       If there is an issue, set result['message'] to a string containing the line number
-       and description of the issue */
-
         $correct = true;
         $emptyLine = false;
-        $lineArray = mb_split("\n", $file_string);      //will hold an array of strings separted by newlines
-        $store = array();                               //will be used to store database entries
+		
+        // Separate each line of the file into an array
+        $lineArray = mb_split("\n", $file_string);
+		
+		// Used to store database entries
+        $store = array();
         $result = array("status" => "", "message" => "");
         $storeCount = 0; 
          
         for ($i = 0; $i < count($lineArray) ; $i++)
         {
-            //Grab each string separated by spaces
-            
+            //Grab each string separated by spaces   
             $wordArray[$i] = mb_split (" ", $lineArray[$i]);
             if (count($wordArray[$i]) > 1 )
             {
@@ -36,18 +52,16 @@ class Conflict_Time extends Eloquent {
                     {
                         $store [$storeCount][0] = $wordArray[$i][0];
                     
-                        //________________________________________________________
-                        // CHECKING FOR CORRECT COURSE NAME
-                        //________________________________________________________
-                        
+                        // Checking for correct course name
                         if (!mb_ereg_match('^[A-Z]{2,5}\d{3}[A-Z]{0,2}$', $wordArray[$i][0]))
                         {    
                          
                             $correct = false; 
                             $result['status'] = 'error';
-                            $result['message'] = $result['message'] . "\nError with course name on line " . ($i+1) . '.';
-                            break;
-                           
+                            $result['message'] = $result['message'] .
+                            "\nError with course name on line " . ($i+1) . '.';
+							
+                            break; 
                         }
                         
                     }
@@ -62,20 +76,18 @@ class Conflict_Time extends Eloquent {
                         
                         
                         
-                        //If the combination has a slash it will enter into this loop
+                        // If the combination has a slash it will enter into this loop
                         if (!mb_ereg_match('^[^:]*$', $tempword[0]))
                         {
                                                                   
                             while ($tempword[0][$tempcount] != "/" && $tempcount < strlen($tempword[0]) )
                             {
-                                //gather all the days
-                                
+                                // Gather all the days
                                 $days[$tempcount] = $tempword[0][$tempcount];    
                                 $tempcount++; 
                             }
                             
-                            //skip over the slash
-                            
+                            // Skip over the slash
                             $tempcount++;
                             $count = 0; 
                             
@@ -90,15 +102,11 @@ class Conflict_Time extends Eloquent {
                             
                             }
                             
-                            //_________________________________________________________________________
-                            // CHECKING FOR CORRECT TIME
+                            // Checking for correct time
                             // ***Time has to be between 07:00 and 18:30
-                            //_________________________________________________________________________
-                            
                             if (strlen($time) == 5 )
                             {
-                                //ex 09:00
-                               
+                                // ex: 09:00
                                 if ($time[0] == 0 )
                                 {
                                 
@@ -111,7 +119,8 @@ class Conflict_Time extends Eloquent {
                                             if ($time[4] == 3 && $time[1] == 8) 
                                             {
                                                 $result["status"] = "error";
-                                                $result["message"] = $result["message"] . "\nError with time on line " . ($i+1) . '.';
+                                                $result["message"] = $result["message"] .
+                                                "\nError with time on line " . ($i+1) . '.';
                                                 $correct = false;
                                             }
                                             
@@ -122,13 +131,13 @@ class Conflict_Time extends Eloquent {
                                     else
                                     {
                                         $result["status"] = "error";
-                                        $result["message"] = $result["message"] . "\nError with time on line " . ($i+1) . '.';
+                                        $result["message"] = $result["message"] .
+                                        "\nError with time on line " . ($i+1) . '.';
                                         $correct = false;
                                     } 
                                 }
                                 
                                 // ex: 10:00
-                                 
                                 elseif ($time[0] == 1)
                                 {    
                                 
@@ -141,14 +150,16 @@ class Conflict_Time extends Eloquent {
                                             if ($time[4] == 3 )
                                             {
                                                 $result["status"] = "error";
-                                                $result["message"] = $result["message"] . "\nError with time on line " . ($i+1) . '.';
+                                                $result["message"] = $result["message"] .
+                                                "\nError with time on line " . ($i+1) . '.';
                                                 $correct = false;
                                             }
 
                                             elseif ( $time[1] == 8 && $time[3] == 3)
                                             {
                                                 $result["status"] = "error";
-                                                $result["message"] = $result["message"] . "\nError with time on line " . ($i+1) . '.';
+                                                $result["message"] = $result["message"] .
+                                                "\nError with time on line " . ($i+1) . '.';
                                                 $correct = false;
                                             }
                                             
@@ -159,24 +170,24 @@ class Conflict_Time extends Eloquent {
                                     else
                                     {
                                         $result["status"] = "error";
-                                        $result["message"] = $result["message"] . "\nError with time on line " . ($i+1) . '.';
+                                        $result["message"] = $result["message"] .
+                                        "\nError with time on line " . ($i+1) . '.';
                                         $correct = false;
                                     }
                                     
                                 }
 
                                 // False if the time does not fall between 10:00 and 18:00
-
                                 elseif($time[0] == 2)
                                 {
                                     $result["status"] = "error";
-                                    $result["message"] = $result["message"] . "\nError with time on line " . ($i+1) . '.';
+                                    $result["message"] = $result["message"] .
+                                    "\nError with time on line " . ($i+1) . '.';
                                     $correct = false;
                                 }
 
-                                //If it has more than one conflict time, this loop
-                                // will be entered.
-                          
+                                // If it has more than one conflict time, this loop
+                                // will be entered. 
                                 if ($j > 1)
                                 {
                                                                                            
@@ -195,16 +206,12 @@ class Conflict_Time extends Eloquent {
                             else
                             {
                                 $result["status"] = "error";
-                                $result["message"] = $result["message"] . "\nError with time on line " . ($i+1) . '.';
+                                $result["message"] = $result["message"] .
+                                "\nError with time on line " . ($i+1) . '.';
                                 $correct = false;
                             }
-                            //______________________________________________________________________
-                            //   END OF CHECKING FOR CORRECT TIME
-                            //_______________________________________________________________________
                             
-                            
-                            //setting the tables value days to 0.
-                            
+                            // Setting the tables value days to 0.
                             $store[$storeCount][2] = 0; 
                             $store[$storeCount][3] = 0;
                             $store[$storeCount][4] = 0;
@@ -212,13 +219,14 @@ class Conflict_Time extends Eloquent {
                             $store[$storeCount][6] = 0;
                             $store[$storeCount][7] = 0;                
                             
-                            //______________________________________________________________________
-                            //CHECKING FOR CORRECT DAYS
-                            //_______________________________________________________________________
-                                for ( $dayCount = 0; $dayCount < count($days) && count($days) <= 6 ; $dayCount++)
+
+                                // Checking for correct days
+                                for ( $dayCount = 0; $dayCount < count($days) &&
+                                    count($days) <= 6 ; $dayCount++)
                                 {
-                                    if ($days[$dayCount] == 'M' || $days[$dayCount] == "T" || $days[$dayCount] == "W" || $days[$dayCount] == "R" 
-                                    || $days[$dayCount]== "F" || $days[$dayCount] == "S")
+                                    if ($days[$dayCount] == 'M' || $days[$dayCount] == "T" ||
+                                        $days[$dayCount] == "W" || $days[$dayCount] == "R" ||
+                                        $days[$dayCount]== "F" || $days[$dayCount] == "S")
                                     {
 
                                         if ($days[$dayCount] == "M")
@@ -253,11 +261,12 @@ class Conflict_Time extends Eloquent {
                                         
                                     }
                                     
-                                    //not a correct day
+                                    // Not a correct day
                                     else
                                     {
                                         $result["status"] = "error";
-                                        $result["message"] = $result["message"] . "\nError with days on line " . ($i+1) . '.';
+                                        $result["message"] = $result["message"] .
+                                        "\nError with days on line " . ($i+1) . '.';
                                         $correct = false;
                                     }
                                 
@@ -265,12 +274,13 @@ class Conflict_Time extends Eloquent {
                                 } 
                         }
                         
-                        //False if missing a slash
+                        // False if missing a slash
                         else
                         {
                             
                             $result["status"] = "error";
-                            $result["message"] = $result["message"] . "\nMissing slash on line " . ($i+1) . '.';
+                            $result["message"] = $result["message"] .
+                            "\nMissing slash on line " . ($i+1) . '.';
                             $correct = false;
                             break; 
                         }
@@ -278,10 +288,11 @@ class Conflict_Time extends Eloquent {
                 }
             }
             
-            else{
-            
+            else
+            {
                 $result["status"] = "error";
-                $result["message"] = $result["message"] . "\nPlease delete empty line on line number " . ($i+1) . '.';
+                $result["message"] = $result["message"] .
+                "\nPlease delete empty line on line number " . ($i+1) . '.';
                 $correct = false;
                 $emptyLine = true;
                 //break;
@@ -303,13 +314,16 @@ class Conflict_Time extends Eloquent {
                 if ( $j != $i)
                 {
                 
-                    if ($store[$j][0] == $temp[0] && $store[$j][1] == $temp[1] && $store[$j][2] == $temp[2] && $store[$j][3] == $temp[3] 
-                        && $store[$j][4] == $temp[4] && $store[$j][5] == $temp[5] && $store[$j][6] == $temp[6] && $store[$j][7] == $temp[7])
+                    if ($store[$j][0] == $temp[0] && $store[$j][1] == $temp[1] &&
+                        $store[$j][2] == $temp[2] && $store[$j][3] == $temp[3] &&
+                        $store[$j][4] == $temp[4] && $store[$j][5] == $temp[5] &&
+                        $store[$j][6] == $temp[6] && $store[$j][7] == $temp[7])
                     {
                         $correct = FALSE;
                         $duplicate = true;
                         $result['status'] = "error";
-                        $result['message'] =  $result['message']  . "\nPlease remove duplicates" . '. ';
+                        $result['message'] =  $result['message']  .
+                        "\nPlease remove duplicates" . '. ';
                     }
                     
                 }
@@ -347,7 +361,20 @@ class Conflict_Time extends Eloquent {
         }
         return $result;  
     }
-
+    //*************************************************************************
+    //* End of scan function
+    //*************************************************************************
+    
+    /**************************************************************************
+    /* @function    get_text
+    /* @author      Atticus Wright
+    /* @description This segment of code will retreive the contents of the 
+    /*              text of the class time database entries.
+    /* @input       $schedule_id -> the identification number of the schedule
+    /*              currently being created. This value is used when inputing
+    /*              to and extracting from the database.
+    /* @output      $text -> A string of the information for an entry.
+    /*************************************************************************/
     public static function get_text($schedule_id)
     {
 
@@ -378,7 +405,17 @@ class Conflict_Time extends Eloquent {
 
         return $text;
     }
-
+    //*************************************************************************
+    //* End of get_text function
+    //*************************************************************************
+  
+    /**************************************************************************
+    /* @function    get_days_string
+    /* @author      Atticus Wright
+    /* @description This segment of code will
+    /* @input       $entry ->
+    /* @output      $result ->
+    /*************************************************************************/
     public static function get_days_string($entry){
 
         $result = "";
